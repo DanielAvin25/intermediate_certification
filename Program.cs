6631974738b68a7ea4935c09b4d8641879ea3644
Daniel_Avinerov <danavinerov@yandex.ru>
2024-02-18 16:59:01 +0100

    3 этап

--- Program.cs
+++ Program.cs
@@ -7,3 +7,14 @@ foreach (string str in Array)
         count++;
     }
 }
+
+string[] NewArray = new string[count];
+int index = 0;
+foreach (string str in Array)
+{
+    if (str.Length <= 3)
+    {
+        NewArray[index] = str;
+        index++;
+    }
+}
